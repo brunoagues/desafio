3d12659c99b50bb0eb42016671337ebaae4f4605
brunoagues <bruno2k@gmail.com>
2019-12-24 16:47:08 -0300

    adcionando repositorio jdbc repositorio perfil

---
+++ src/main/java/br/com/desafio/usuario/repository/PerfilRepositoryJDBC.java
@@ -0,0 +1,10 @@
+package br.com.desafio.usuario.repository;
+
+import br.com.desafio.usuario.model.Perfil;
+
+public interface PerfilRepositoryJDBC {
+
+	public void save(Perfil perfil) throws Exception;
+
+	
+}
